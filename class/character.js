807b3a954da721f4723a4ca33fd902e76fa9f9f9
Mafe Garcia <mfg21390@gmail.com>
2023-06-09 15:56:49 -0400

    init

---
+++ class/character.js
@@ -0,0 +1,22 @@
+class Character {
+
+  constructor(name, description, currentRoom) {
+    this.name = name
+    this.description = description
+    this.currentRoom = currentRoom
+
+  }
+
+  applyDamage(amount) {
+    // Fill this in
+  }
+
+  die() {
+    // Fill this in
+  }
+
+}
+
+module.exports = {
+  Character,
+};
